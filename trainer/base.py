96754fadf6dcc2be032b24bbe3a6902c6502c786
WW <924544296@qq.com>
2023-07-12 18:03:42 +0800

    1

--- trainer/base.py
+++ trainer/base.py
@@ -14,17 +14,11 @@ class Trainer:
     def trainer_g(self):
         pass 
     #
+    def save_image(self):
+        pass 
+    #
     def train(self):
         #
-        # net_d = Discriminator()
-        # net_d.train()
-        # net_g = Generator()
-        # net_g.train()
-        # optimizer_d = P.optimizer.Adam(parameters=net_d.parameters(), learning_rate=learning_rate, beta1=0.5)
-        # optimizer_g = P.optimizer.Adam(parameters=net_g.parameters(), learning_rate=2*learning_rate, beta1=0.5)
-        if self.opt.load_model:
-            self.net_d.set_state_dict(P.load(self.opt.path_result + 'net_d.pdparams'))
-            self.net_g.set_state_dict(P.load(self.opt.path_result + 'net_g.pdparams'))
         #
         iteration = 0
         for epoch in range(self.opt.epochs):
@@ -36,9 +30,9 @@ class Trainer:
                 if iteration % self.opt.step_g == 0:
                     self.trainer_g(image)
         #
-                if iteration % 1000 == 0:
+                if iteration % self.opt.sample_interval == 0:
                     print('Epoch: ', epoch, ', Iteration: ', iteration)
                     P.save(self.net_d.state_dict(), self.opt.path_result + 'net_d.pdparams')
                     P.save(self.net_g.state_dict(), self.opt.path_result + 'net_g.pdparams')
-                    # show(net_g)
+                    self.save_image(epoch, iteration)
                     self.net_g.train()
--- trainer/gan.py
+++ trainer/gan.py
@@ -1,5 +1,6 @@
 import paddle as P 
 import paddle.nn.functional as F 
+import cv2 
 from network.gan import Generator_GAN, Discriminator_GAN 
 from dataloader.Dataset_ import Dataset_GAN 
 from trainer.base import Trainer 
@@ -13,6 +14,9 @@ class Trainer_GAN(Trainer):
         self.net_d.train()
         self.net_g = Generator_GAN(opt.dim_latent, opt.channel_g)
         self.net_g.train()
+        if self.opt.load_model:
+            self.net_d.set_state_dict(P.load(self.opt.path_result + 'net_d.pdparams'))
+            self.net_g.set_state_dict(P.load(self.opt.path_result + 'net_g.pdparams'))
         self.optimizer_d = P.optimizer.Adam(parameters=self.net_d.parameters(), 
                             learning_rate=opt.learning_rate, beta1=opt.beta1, beta2=opt.beta2)
         self.optimizer_g = P.optimizer.Adam(parameters=self.net_g.parameters(), 
@@ -42,6 +46,13 @@ class Trainer_GAN(Trainer):
         self.optimizer_g.clear_grad()
         loss.backward()
         self.optimizer_g.step()
+    #
+    def save_image(self, epoch, iteration): 
+        self.net_g.eval() 
+        image = self.net_g(P.randn([8, self.opt.dim_latent, 1, 1])) 
+        image = image.reshape([2,4,3,64,64]).transpose([0,3,1,4,2]).reshape([128,256,3]).numpy()
+        cv2.imwrite('%s Epoch %d, Iteration %d' % (self.opt.result, epoch, iteration), image)
+        self.net_g.train() 
     #
     def train(self):
         super().train()
